8663d37f64c19a6d9112f5b9f06a08a883f7faf8
Maria Clara Amorim <mariaclaraamorim221@gmail.com>
2024-05-10 10:28:07 -0300

    feat: add componet star Badge

---
+++ app/_components/star.tsx
@@ -0,0 +1,25 @@
+import { StarIcon } from "lucide-react";
+import { cn } from "../_lib/utils";
+import { Restaurant } from "@prisma/client";
+
+interface StarBadgeProps {
+  restaurant: Pick<Restaurant, "stars">;
+  className?: string;
+}
+
+export default function StarBadge({ restaurant, className }: StarBadgeProps) {
+  return (
+    <div
+      className={cn(
+        "flex items-center gap-0.5 rounded-full bg-background px-2 py-0.5 text-accent-foreground dark:border-accent",
+        className,
+      )}
+    >
+      <StarIcon size={16} className="fill-yellow-400 text-yellow-400" />
+
+      <span className="text-xs font-semibold">
+        {Number(restaurant.stars).toFixed(1)}
+      </span>
+    </div>
+  );
+}
